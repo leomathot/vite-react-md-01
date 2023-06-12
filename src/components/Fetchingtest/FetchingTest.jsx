a9da43dcf304d3c09e36bc6682bceb8932ddb763
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-06-12 20:04:26 -0300

    Add cat fact fetching

--- src/components/Fetchingtest/FetchingTest.jsx
+++ src/components/Fetchingtest/FetchingTest.jsx
@@ -1,25 +1,48 @@
 import { useEffect, useState } from "react"
 
 
-const catsFactsUrl = "https://catfact.ninja/fact"
-const catsImagesUrl = "https://cataas.com/cat/says/hello"
+const CAT_FACTS_URL = "https://catfact.ninja/fact"
+// const CAT_IMAGES_URL = "https://cataas.com/cat/says/hello"
 
 export default function FetchingTest() {
 
     const [fact, setFact] = useState()
+    const [fact2, setFact2] = useState()
 
     useEffect(() => {
-        fetch(catsFactsUrl)
-            .then(response => response.json())
-            .then(data => setFact(data.fact))
+        fetch(CAT_FACTS_URL)
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+
+                const firstWord = fact.split(" ")[0]
+                console.log(firstWord)
+            })
+
+        // fetching 2 with async await
+        async function fetchFact2() {
+            const res2 = await fetch(CAT_FACTS_URL)
+            const json2 = await res2.json()
+            setFact2(json2.fact)
+        }
+        fetchFact2()
+
     }, [])
 
     return (
         <article className="app-article" id="fetching-test">
             <h3>Fetching test</h3>
             <main>
-                {fact && <p>{fact}</p>}
-                <button>New fact</button>
+                <div className="fact-cont">
+                    {fact && <p>{fact}</p>}
+                    <button>New fact</button>
+                </div>
+
+                <div className="fact-cont">
+                    {fact2 && <p>{fact2}</p>}
+                    <button>New fact</button>
+                </div>
             </main>
         </article>
     )
