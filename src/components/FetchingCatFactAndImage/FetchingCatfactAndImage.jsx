4bc2141720ad83cd29ebcfd71c5d836c2447bfc0
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-06-19 01:21:39 -0300

    Change test selectors

--- src/components/FetchingCatFactAndImage/FetchingCatfactAndImage.jsx
+++ src/components/FetchingCatFactAndImage/FetchingCatfactAndImage.jsx
@@ -12,8 +12,8 @@ export default function FetchingCatFactAndImage() {
             <h3>Fetching Cat Fact and Image</h3>
             <main>
                 <div className="fact-cont">
-                    {fact && <p>{fact}</p>}
-                    <img src={imageUrl} alt={`Cat photo fetched using the longest word in the following random cat fact: ${fact}`}/>
+                    {fact && <p data-testid="cat-fact">{fact}</p>}
+                    <img src={imageUrl} data-testid="cat-img" alt={`Cat photo fetched using the longest word in the following random cat fact: ${fact}`}/>
                     <button className="get-fact-btn" onClick={getNewFact}>New fact</button>
                 </div>
             </main>
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,12 +7,12 @@ const CAT_PREFIX_IMAGE_URL = "https://cataas.com/cat/says/"
 test('fetching cat fact and image component shows random fact and image', async ({ page }) => {
   await page.goto(LOCAL_HOST_URL);
 
-  const text = await page.getByRole("paragraph")
-  const image = await page.getByRole("img")
+  const text = await page.getByTestId("cat-fact")
+  const image = await page.getByTestId("cat-img")
 
   const textContent = await text.textContent()
   const imageSrc = await image.getAttribute("src")
 
   await expect(textContent?.length).toBeGreaterThan(0)
-  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
+  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy
 });
